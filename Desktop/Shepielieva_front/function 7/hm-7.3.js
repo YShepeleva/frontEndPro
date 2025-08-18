c5cedc41b967b326d4c4d7ed6707cff65b25fdd1
Yana Shepielieva <yanashepeleva@MacBook-Air-Yana.local>
2025-08-18 14:05:49 +0200

    rework 7.3

--- Desktop/Shepielieva_front/function 7/hm-7.3.js
+++ Desktop/Shepielieva_front/function 7/hm-7.3.js
@@ -6,20 +6,34 @@
 // функція виводить в консоль останній введення користувача 
 // і завершує функцію.
 
-let i = 0;
+// let i = 0;
 
-function enterNum() {
-    let number = parseInt(prompt('Enter the number'));
-    if (number < 100 || isNaN(number)) {
-        i++;
-        if (i > 10) {
-            document.write(`You are out of tries. Your last number is ${number}`);
-            return;
-        }
-        enterNum();
-    } else {
-        document.write(`Your last number is ${number}`);
-    }
+// function enterNum() {
+//     let number = parseInt(prompt('Enter the number'));
+//     if (number < 100 || isNaN(number)) {
+//         i++;
+//         if (i > 10) {
+//             document.write(`You are out of tries. Your last number is ${number}`);
+//             return;
+//         }
+//         enterNum();
+//     } else {
+//         document.write(`Your last number is ${number}`);
+//     }
+// }
+
+// enterNum();
+
+//--решение с циклом--
+
+for (let i = 0; i < 10; i++) {
+    let value = prompt('Enter the number > 100');
+
+    if (value > 100 || isNaN(value) || i === 9) {
+        console.log(`User has entered ${value}`);
+        break; 
+    } else if (value <= 100) {
+        console.log('Enter the correct number');
+    } 
 }
 
-enterNum();
